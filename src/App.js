540db9727bd06a063798612c9dab5aff1d4c098d
Davitmach <wvime30@gmail.com>
2025-03-22 16:23:41 +0400

    de

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Home } from './page/home/page';
 import Footer from './components/shared/footer/footer';
 import Header from './components/shared/header/header';
 import { Politika } from './page/politika/page';
 import { Case } from './page/case/page';
+import { useEffect } from 'react';
+
+function ScrollToTop() {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <ScrollToTop />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
